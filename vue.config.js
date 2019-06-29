a92dfb56bc379a75217507e902f770eb47c690bb
MoonCheung <Salvador23@163.com>
2019-06-30 00:47:35 +0800

    modif main.js 按需引入

--- vue.config.js
+++ vue.config.js
@@ -23,7 +23,8 @@ module.exports = {
   outputDir: 'dist', // 打包目录
   assetsDir: 'static',
   indexPath: 'index.html',
-  lintOnSave: process.env.NODE_ENV === 'development',
+  // lintOnSave: process.env.NODE_ENV === 'development',
+  lintOnSave: false,
   productionSourceMap: false,
   devServer: {
     port: port,
