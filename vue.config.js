03ccd72894fc7f588ce17f0d33fcb380f1811251
MoonCheung <salvador23@163.com>
2019-11-12 23:25:04 +0800

    del: mini-css-extract-plugin 依赖包

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,6 @@
 'use strict';
 const CompressionPlugin = require('compression-webpack-plugin');
 const AddAssetHtmlPlugin = require('add-asset-html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
 const defaultSettings = require('./src/settings.js');
 const webpack = require('webpack');
@@ -125,45 +124,13 @@ module.exports = {
         'https://unpkg.com/element-ui@2.7.2/lib/index.js'
       ]
     };
-    config.plugins.delete('preload'); // TODO: need test
+    config.when(process.env.NODE_ENV === 'development', config => {
+      config.plugins.delete('preload') // TODO: need test
+    });
     config.plugins.delete('prefetch'); // TODO: need test
 
     // 添加别名
     config.resolve.alias.set('@', resolve('src'));
-
-    // 提取到css文件中(暂时研究)
-    config.merge({
-      module: {
-        rules: [{
-          test: /\.(css|sass|scss)$/,
-          use: [
-            MiniCssExtractPlugin.loader,
-            {
-              loader: 'css-loader',
-              options: {
-                importLoaders: 2,
-                sourceMap: true
-              }
-            },
-            {
-              loader: 'postcss-loader',
-              options: {
-                plugins: () => [
-                  require('autoprefixer')
-                ],
-                sourceMap: true
-              }
-            },
-            {
-              loader: 'sass-loader',
-              options: {
-                sourceMap: true
-              }
-            }
-          ]
-        }]
-      }
-    })
 
     // set svg-sprite-loader
     config.module
@@ -228,19 +195,9 @@ module.exports = {
             minChunks: 3, //  minimum common number
             priority: 5,
             reuseExistingChunk: true
-          },
-          css: {
-            test: /\.(css|sass|scss)$/,
-            name: "commons",
-            minChunks: 2,
           }
         }
       });
-      config.plugin('extract-css')
-        .use(MiniCssExtractPlugin, [{
-          filename: 'static/css/[name].css',
-          allChunks: true
-        }]);
       config.optimization.runtimeChunk('single');
     });
 
