db8beb54bf2a88207fa79f1ce4e15dd99adcb777
MoonCheung <salvador23@163.com>
2020-03-14 23:04:14 +0800

    chore: create comment file

---
+++ src/api/comment.js
@@ -0,0 +1,8 @@
+/*
+ * @Description: 文章评论API
+ * @Author: MoonCheung
+ * @Github: https://github.com/MoonCheung
+ * @Date: 2020-03-14 23:01:32
+ * @LastEditors: MoonCheung
+ * @LastEditTime: 2020-03-14 23:02:55
+ */
